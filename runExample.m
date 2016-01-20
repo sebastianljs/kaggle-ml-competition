3aa6e32bc8b3e9145040dcc783674380d5ad53b7
kylebradbury <kbradbury@gmail.com>
2016-01-19 22:31:53 -0500

    Completed the demo code

--- runExample.m
+++ runExample.m
@@ -15,49 +15,108 @@
 % Testbed for exploring machine learning techniques
 %--------------------------------------------------------------------------
 
-% -- Load data and initialize parameters -----------------------------
+% -- Load training data and initialize parameters --------------------
 % Initialize parameters
-imageDirectory    = './train/' ;       % Adjust this directory to point to your image files
-solutionDirectory = './solution/' ;    % Adjust this directory to point to your training label/solution files
-kFolds            = 5 ;                % Number of folds for cross validation
-submissionFile    = 'submission.csv' ; % Output csv submission filename
+imageDirectoryTraining  = './train/' ;   % Adjust this directory to point to your image files for training
+trainingLabelsDirectory = './solution/' ;% Adjust this directory to point to your training label/solution files
+kFolds                  = 10 ;               % Number of folds for cross validation
 
 % Load the training data solution
-labelsRaw = csvread([solutionDirectory 'train_solution.csv'],1,0) ; % Extract the class labels
-labels = sortrows(labelsRaw,1) ; % In case their not in ascending order, sort them
-labels = labels(:,2) ; % Remove the id since they are now in order and therefore can be indexed by their ids
+trainingLabelsRaw = csvread([trainingLabelsDirectory 'train_solution.csv'],1,0) ; % Extract the class labels
+trainingLabels = sortrows(trainingLabelsRaw,1) ; % In case their not in ascending order, sort them
+trainingLabels = trainingLabels(:,2) ; % Remove the id since they are now in order and therefore can be indexed by their ids
 
-nObservations = length(labels) ; % Total number of observations
+nObservations  = length(trainingLabels) ; % Total number of observations
+
+% Initialize the storage vectors
+trainingFeatures = nan(nObservations,2) ; % You'll want to adjust this as you adjust the number of features
+trainingDecision = nan(nObservations,1) ;
+trainingScores   = nan(nObservations,1) ;
+
 
 % -- Extract feature vectors -----------------------------------------
 for iObservation = 1:nObservations
     % Load each image
-    cImage = imread(sprintf('%simage%g.tif',imageDirectory,iObservation),'tif') ;
+    cImage = imread(sprintf('%simage%g.tif',imageDirectoryTraining,iObservation),'tif') ;
     
     % Extract features from the image
-    features(1,2) = [mean(cImage(:)) std(cImage(:))] ;
+    cVectorImage = double(cImage(:)) ;
+    trainingFeatures(iObservation,:) = [mean(cVectorImage) std(cVectorImage)] ;
 end
 
 % -- Conduct cross-validated training of the data --------------------
 partition = cvpartition(nObservations,'KFold',kFolds) ; % This creates a cvpartition class object for the cross validation
 
 for iFold = 1:kFolds % For each fold of the partition
+    % Get the indices of the current partition
+    iTrain = partition.training(iFold) ;
+    iTest  = partition.test(iFold) ;
     
+    % Extract the current partition
+    cTrainingFeatures = trainingFeatures(iTrain,:) ;
+    cTrainingLabels   = trainingLabels(iTrain) ;
+    cTestFeature      = trainingFeatures(iTest,:) ;
+    
+    % Train the classifier on the training data
+    trainedModel = fitcknn(cTrainingFeatures,cTrainingLabels,...
+                            'NumNeighbors',5) ;
+    
+    % Test the trained classifier on the test data
+    [cTestLabels,cTestScores] = predict(trainedModel,cTestFeature) ;
+    
+    % Store the decisions and scores
+    trainingDecision(iTest) = cTestLabels ;
+    trainingScores(iTest)   = cTestScores(:,2) ;
 end
 
-
-
-
-% Display ROC curve
-
+% -- Compute and display ROC curve -----------------------------------
+positiveClass = 1 ; % This is the "positive class" or the "target" class
+[xRoc,yRoc,thresholdRoc,aucRoc] = perfcurve(trainingLabels,trainingScores,1) ;
+h(1) = plot([0 1],[0 1],'k--') ; hold on ; % Plot the chance diagonal
+h(2) = plot(xRoc,yRoc,'r-') ; % Plot the ROC
+xlabel('Probability of False Alarm')
+ylabel('Probability of Correct Detection')
+legend(h,'Chance Diagonal','Performance',...
+        'Location','southeast')
+title(sprintf('ROC Curve (AUC = %g)',aucRoc)) ;
 
 %--------------------------------------------------------------------------
 % Train classifier on training data and run on test data to produce results
 % for submission to Kaggle
 %--------------------------------------------------------------------------
+% Since the sample submission file has a complete list of image ids, we can
+% use that to be our index
+submissionSampleFile    = './sampleSubmissions/sampleSubmission.csv' ; % Adjust this to point to your sample submission file
+imageDirectoryTesting   = './test/' ;    % Adjust this directory to point to your image files for testing
+submissionFile          = 'submission.csv' ; % Output csv submission filename
+
+% -- Load test data and initialize parameters ------------------------
+testIds = csvread(submissionSampleFile,1,0) ; % Extract the class labels
+testIds = sort(testIds(:,1)) ; % In case their not in ascending order, sort them
+nTestObservations = length(testIds) ;
+testingFeatures   = nan(nTestObservations,2) ;
+
+% -- Extract feature vectors -----------------------------------------
+for iObservation = 1:nTestObservations
+    % Load each image
+    cImage = imread(sprintf('%simage%g.tif',imageDirectoryTesting,testIds(iObservation)),'tif') ;
+    
+    % Extract features from the image
+    cVectorImage = double(cImage(:)) ;
+    testingFeatures(iObservation,:) = [mean(cVectorImage) std(cVectorImage)] ;
+end
 
 % Train the classifier on all of the training data
+trainedModel = fitcknn(trainingFeatures,trainingLabels,'NumNeighbors',5) ;
 
 % Run the classifier on the test data
+[testLabels,testScores] = predict(trainedModel,testingFeatures) ;
+testScores = testScores(:,2) ; % The second column generally has the test scores for the target class ;
 
-% Output the results
+% Output the results to csv file
+fid = fopen(submissionFile, 'w') ;
+fprintf(fid,'%s,%s\n','id','class') ;
+fclose(fid) ;
+
+output = [testIds testScores] ;
+dlmwrite(submissionFile, output, '-append') ;
